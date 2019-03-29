f28c1aa950796b59df3e1f229c9b05f4b3c52d83
iwillreku3206 <iwillreku3206@gmail.com>
2019-03-29 19:18:29 +0800

    Create menus.lua

---
+++ menus.lua
@@ -0,0 +1,15 @@
+
+
+
+
+term.setBackgroundColor(colors.black)
+term.clear()
+
+while true do
+    local event,button,x,y = os.pullEvent("mouse_click")
+
+    term.setBackgroundColor(colors.black)
+    term.setTextColor(colors.write)
+    term.setCursorPos(1,1)
+    term.write("Toggle Menu")
+end
