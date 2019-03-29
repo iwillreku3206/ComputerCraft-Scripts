cca6e233dc73f9d389d682ca4ab682a6bc48d148
iwillreku3206 <iwillreku3206@gmail.com>
2019-03-29 19:31:27 +0800

    Update menus.lua

--- menus.lua
+++ menus.lua
@@ -2,12 +2,23 @@
 term.setBackgroundColor(colors.black)
 term.clear()
 
+function updateScreen () = 
+    {
+        term.clear()
+        term.setTextColor(colors.white)
+        term.setCursorPos(2, 2)
+        term.write("Toggle Menu")
+
+        term.setCursorPos (2, 5)
+        term.write(x)
+        term.setCursorPos (2, 6)
+        term.write(y)
+    
+    }
+
 while true do
     local event,button,x,y = os.pullEvent("mouse_click")
+    updateScreen()
 
-    term.setTextColor(colors.white)
-    term.setCursorPos(2, 2)
-    term.write("Toggle Menu")
 
-    term.clear()
 end
